9d3f160b3c24cb2b3a0b02cb9d50b9373b146523
Pavel Milanes (CO7WT) <dev0032@simelo.tech>
2018-10-30 16:22:39 -0400

    Fixing the remote part, now working WITHOUT auth

--- get-remote.sh
+++ get-remote.sh
@@ -10,22 +10,25 @@
 repo_manifest=$(mktemp)
 repo_config=$(mktemp)
 
-# local vars
+# vars
 image=""
 tag=""
+token=""
 
 # Address of the registry that we'll be performing the inspections against.
 # This is necessary as the arguments we supply to the API calls don't include 
 # such address (the address is used in the url itself).
 # If you use the docker hub, this is fine, if you use a private registry you need
 # to modify it (just the fqdn of the registry, without the protocol) 
-readonly REGISTRY_ADDRESS="${registry-1.docker.io}"
+# fixed use default CDN address (watch out for the -L with curl!)
+readonly REGISTRY_ADDRESS="https://registry.hub.docker.com/v2"
+readonly ENCODING="application/vnd.docker.distribution.manifest.v2+json"
 
 # if you are behind a http proxy uncoment and modify as needed
 # export http_proxy="http://localhost:8118/"
 # export https_proxy="http://localhost:8118/"
 
-# shot to warn about jq, seed & cut absence on the system
+# shot to warn about jq, sed & cut absence on the system
 jq --help &> /dev/null
 sed --help &> /dev/null
 cut --help &> /dev/null
@@ -35,8 +38,7 @@ cut --help &> /dev/null
 # (image_name and image_tag) and then performs the main workflow:
 #       1.      get the auth tocken to access the img on the registry
 #       2.      retrieve the image digest
-#       3.      retrieve the configuration for
-#               that digest.
+#       3.      retrieve the configuration for that digest.
 main() {
 	check_args "$@"
 
@@ -44,39 +46,12 @@ main() {
 	tag=$2
 
 	#  TODO test this online with a private image
-	local token=$(get_token $image:$tag)
-	local digest=$(get_digest $image $tag $token)
-	get_image_configuration $image $digest
-
-	# local testing 
-	# local digest=$(get_digest $image $tag)
-	# get_image_configuration $image $digest
+	local token=$(get_token $image $tag)
+	local digest=$(get_digest)
+	get_image_configuration $digest
 
 	# parse the obtained data
 	parse_data
-}
-
-
-# Retrieves a token that grants access to a private image
-# named `image` on registry.docker.io.
-# note.:        the user identified by `DOCKER_USERNAME`
-#               and `DOCKER_PASSWORD` must have access
-#               to the image.
-get_token() {
-	local image=$1
-
-	# ask for user:password to access the docker image
-	echo "As the images in the registry are privated ones you need to supply here"
-	echo "a username and password of docker hub in order to get the needed info."
-	echo "" 
-	read -p "User: " DOCKER_USERNAME
-	read -p "Password: " DOCKER_PASSWORD
-
-	curl \
-		--silent --anyauth \
-		-u "$DOCKER_USERNAME:$DOCKER_PASSWORD" \
-		"https://auth.docker.io/token?scope=repository:$image:pull&service=registry.docker.io" \
-		| jq -r '.token'
 }
 
 
@@ -97,34 +72,48 @@ check_args() {
 }
 
 
-# Retrieve the digest, now specifying in the header the token we just received.
-# note.:        $token corresponds to the token
-#               that we received from the `get_token`
-#               call.
-# note.:        $image must be the full image name without
-#               the registry part, e.g.: `nginx` should
-#               be named `library/nginx`.
-# TODO: check if the tag is part of the name?
+# Retrieves the auth token
+# It works only for public accessible images
+# You must pass:
+# 	- repository name (library/ubuntu|skycoin/skycoin)
+#	- tag for this repository (latest|develop)
+get_token() {
+	local image="$1"
+	local tag="$2"
+	local URI="$REGISTRY_ADDRESS/$image/manifests/$tag"
+
+	# first failed try, to capture real, dervice & scope
+	local MANIFEST="`curl -skLG -o /dev/null -D- $URI`"
+	local CHALLENGE="`grep "Www-Authenticate" <<<"$MANIFEST"`"
+
+	# Check for a valid answer
+	if [[ CHALLENGE ]]; then
+		IFS=\" read _ REALM _ SERVICE _ SCOPE _ <<<"$CHALLENGE"
+		echo REALM is $REALM >&2
+		echo SERVICE is $SERVICE >&2
+		echo SCOPE is $SCOPE >&2
+		echo "Got a Valid Auth-token" >&2
+		local TOKEN="`curl -sLG "$REALM?service=$SERVICE&scope=$SCOPE"`"
+		IFS=\" read _ _ _ TOKEN _ <<<"$TOKEN"
+	else
+		# no valid answer
+		echo "No valid answer, exit" >&2
+		exit 1
+	fi
+}
+
+
+# Retrieve the digest ffor the repository image
 get_digest() {
-	local image=$1
-	local tag=$2
-	local token=$3
+	URI="$REGISTRY_ADDRESS/$image/manifests/$tag"
 
-	echo "Retrieving image digest.
-		IMAGE:  $image
-		TAG:    $tag
-		TOKEN:  $token
-	" >&2
-
+	# get manifest
 	local manifest=$(
-		curl \
-		--silent \
-		--header "Accept: application/vnd.docker.distribution.manifest.v2+json" \
-		"http://$REGISTRY_ADDRESS/v2/$image/manifests/$tag"
+		curl -sLG \
+		--header "Accept: $ENCODING" \
+		--header "Authorization: Bearer $token" \
+		"$URI"
 	)
-		
-	# TODO include auth
-	# --header "Authorization: Bearer $token" \
 
 	# dump to a file for debug purposes
 	echo $manifest > $repo_manifest
@@ -138,19 +127,21 @@ get_digest() {
 # See more about the endpoint at:
 # https://docs.docker.com/registry/spec/api/#pulling-a-layer
 get_image_configuration() {
-	local image=$1
-	local digest=$2
+	local URI="$REGISTRY_ADDRESS/$image/blobs/$1"
+	echo "URI=$URI"
 
 	local digest_config=$(
-		curl \
-		--silent \
-		--location \
-		"http://$REGISTRY_ADDRESS/v2/$image/blobs/$digest"
+		curl -vLG \
+		--header "Accept: $ENCODING" \
+		--header "Authorization: Bearer $token" \
+		"$URI"
 	)
 		
-	# dumping the digest config for debug purposes
+	# dumping the digest config
 	echo $digest_config > $repo_config
 
+	# DEBUG
+	echo $digest_config >&2
 }
 
 
@@ -158,7 +149,6 @@ get_image_configuration() {
 # the $output that will be moved to $tag.md in the remote directory 
 parse_data() {
 	# defaults
-	mime="application/vnd.docker.distribution.manifest.list.v2+json"
 
 	# capturing the data
 	# TODO the digest is from maniest.config.digest | repo_config.container
@@ -204,7 +194,7 @@ parse_data() {
 	echo "$ docker pull $image@$digest"
 	echo '```'
 	echo ""
-	echo '- Manifest MIME: `'"$mime"'`'
+	echo '- Manifest MIME: `'"$ENCODING"'`'
 	echo ""
 	echo "- Platform: "
 	echo "	- $os, $arch"
